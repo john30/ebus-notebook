7409e51f1ca017baa41b9246aec1e2cf3f25d492
John <ebusd@ebusd.eu>
2024-06-17 22:32:33 +0200

    switch to esbuild, add new command

--- src/extension.ts
+++ src/extension.ts
@@ -6,6 +6,11 @@ export async function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(
     vscode.workspace.registerNotebookSerializer('ebus-notebook', new Serializer())
   );
+  context.subscriptions.push(vscode.commands.registerCommand('ebus-notebook.createNotebook', async () => {
+		const data = new vscode.NotebookData([new vscode.NotebookCellData(vscode.NotebookCellKind.Code, '', 'typespec')]);
+		const doc = await vscode.workspace.openNotebookDocument('ebus-notebook', data);
+		await vscode.window.showNotebookDocument(doc);
+	}));
   context.subscriptions.push(new Controller());
 }
 
